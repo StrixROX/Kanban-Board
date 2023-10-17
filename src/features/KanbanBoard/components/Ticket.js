52442ad4af3bd7462ac31144450af62e7d8b85c0
StrixROX <40167899+StrixROX@users.noreply.github.com>
2023-10-18 02:14:50 +0530

    Minor visual spacing changes and code cleanup in Ticket component

--- src/features/KanbanBoard/components/Ticket.js
+++ src/features/KanbanBoard/components/Ticket.js
@@ -14,19 +14,19 @@ import priorityUrgent from 'assets/icons/priority-urgent.svg'
 import UserBubble from './Ticket.UserBubble'
 
 const StatusIcons = {
-  'backlog': <FontAwesomeIcon icon={faSpinner} fixedWidth color='#e5e5e5' />,
-  'todo': <FontAwesomeIcon icon={faCircle} fixedWidth color='#e1e1e1' />,
-  'inprogress': <FontAwesomeIcon icon={faCircleHalfStroke} fixedWidth color='#f1ca48' />,
-  'done': <FontAwesomeIcon icon={faCircleCheck} fixedWidth color='#5e6ad2' />,
-  'cancelled': <FontAwesomeIcon icon={faCircleXmark} fixedWidth color='#94a2b3' />,
+  'backlog': <span className="status" title="Backlog"><FontAwesomeIcon icon={faSpinner} fixedWidth color='#e5e5e5' /></span>,
+  'todo': <span className="status" title="Todo"><FontAwesomeIcon icon={faCircle} fixedWidth color='#e1e1e1' /></span>,
+  'inprogress': <span className="status" title="In Progress"><FontAwesomeIcon icon={faCircleHalfStroke} fixedWidth color='#f1ca48' /></span>,
+  'done': <span className="status" title="Done"><FontAwesomeIcon icon={faCircleCheck} fixedWidth color='#5e6ad2' /></span>,
+  'cancelled': <span className="status" title="Cancelled"><FontAwesomeIcon icon={faCircleXmark} fixedWidth color='#94a2b3' /></span>,
 }
 
 const PriorityIcons = {
-  0: <img src={priorityNone} alt="No-priority" />,
-  1: <img src={priorityLow} alt="Low-priority" />,
-  2: <img src={priorityMedium} alt="Medium-priority" />,
-  3: <img src={priorityHigh} alt="High-priority" />,
-  4: <img src={priorityUrgent} alt="Urgent" />,
+  0: <span className="priority" title="No Priority"><img src={priorityNone} alt="No-priority" /></span>,
+  1: <span className="priority" title="Low Priority"><img src={priorityLow} alt="Low-priority" /></span>,
+  2: <span className="priority" title="Medium Priority"><img src={priorityMedium} alt="Medium-priority" /></span>,
+  3: <span className="priority" title="High Priority"><img src={priorityHigh} alt="High-priority" /></span>,
+  4: <span className="priority" title="Urgent!"><img src={priorityUrgent} alt="Urgent" /></span>,
 }
 
 const TagColors = {
@@ -45,34 +45,30 @@ export default function Ticket({ data, userData, groupingScheme }) {
   return (
     <>
       <div className="ticket">
+
         <div className="header">
           <span className="id">{ data.id }</span>
-          {
-            groupingScheme !== 'userId'
-            ? <UserBubble userData={userData} />
-            : null
-          }
+          { groupingScheme !== 'userId' ? <UserBubble userData={userData} /> : null }
         </div>
+
         <div className="details">
-          {
-            groupingScheme !== 'status'
-            ? <span className="status">{StatusIcons[data.status.toLowerCase().replace(' ','')]}</span>
-            : null
-          }
+          { groupingScheme !== 'status' ? StatusIcons[data.status.toLowerCase().replace(' ','')] : null }
           <span className="title">{ clipText(data.title) }</span>
         </div>
+
         <div className="footer">
-          {
-            groupingScheme !== 'priority'
-            ? <span className="priority">{PriorityIcons[data.priority]}</span>
-            : null
-          }
+          { groupingScheme !== 'priority' ? PriorityIcons[data.priority] : null }
           <div className="tags">
             {
-              data.tag.map((el, i) => <span className="tag" style={{ color: TagColors[el.toLowerCase().replace(' ','')] }} key={i}>{el}</span>)
+              data.tag.map((el, i) => {
+                return (
+                  <span className="tag" style={{ color: TagColors[el.toLowerCase().replace(' ','')] }} key={i}>{el}</span>
+                )
+              })
             }
           </div>
         </div>
+
       </div>
     </>
   )
